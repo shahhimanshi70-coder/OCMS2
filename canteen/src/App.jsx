3504a9ea0623bea743c552b2bd759b224aa31e5d
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-18 15:50:51 +0530

    Create App.jsx

---
+++ canteen/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import "./App.css"; 
+import HomePage from "./Components/Layouts/HomePage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      
+    </div>
+  );
+}
+
+export default App;
