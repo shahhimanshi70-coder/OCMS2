3b29c268238b80837fa54556125fc9cc983fcb66
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-18 15:58:04 +0530

    Update App.jsx

--- canteen/src/App.jsx
+++ canteen/src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import "./App.css"; 
 import HomePage from "./Components/Layouts/HomePage";
+import CanteenMenuBar from "./Components/Layouts/CanteenMenuBar";
 
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+       <CanteenMenuBar />
       
     </div>
   );
