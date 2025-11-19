0c07c5d8d049d2ca3c11d472aef7199e1cd65c8b
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-19 15:41:16 +0530

    Create App.jsx

---
+++ canteen/src/Components/Layouts/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar'; 
+import CanteenHome from './CanteenHome';
+import Menu from './Menu'; 
+import Order from './Order'; 
+import Contact from './Contact';
+import './App.css'; 
+
+const App = () => {
+  return (
+    <Router>
+      
+      <Navbar />
+
+      <div className="content">
+        <Routes>
+        
+          <Route path="/" element={<CanteenHome />} /> 
+          <Route path="/menu" element={<Menu />} />  
+          <Route path="/order" element={<Order />} /> 
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Navbar" element={<Navbar/>} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
