5ddca1cfcb0b73bbf11e0110e248aa8ae9284444
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-19 15:41:09 +0530

    Update App.jsx

--- canteen/src/App.jsx
+++ canteen/src/App.jsx
@@ -1,24 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar'; 
+import CanteenHome from './Components/Layouts/CanteenHome';
+import Menu from './Components/Layouts/Menu'; 
+import Order from './Components/Layouts/Order'; 
+import Contact from './Components/Layouts/Contact';
+import './App.css'; 
 
-// App.jsx
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CanteenMenuBar from "./pages/CanteenMenuBar";
-import HomePage from "./pages/HomePage.jsx";
-
-
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <CanteenNavbar />
+    <Router>
+      
+      <Navbar />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/CanteenMenuBar" element={<CanteenMenuBar />} />
-      </Routes>
-    </BrowserRouter>
+      <div className="content">
+        <Routes>
+        
+          <Route path="/" element={<CanteenHome />} /> 
+          <Route path="/menu" element={<Menu />} />  
+          <Route path="/order" element={<Order />} /> 
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/navbar" element={<Navbar/>} />
+
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
-
