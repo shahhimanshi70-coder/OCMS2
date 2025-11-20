6449f659be8da5fcb4077785f41e0e16a77b6e3c
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-20 16:34:04 +0530

    Update Navbar.jsx

--- canteen/src/Components/Navbar.jsx
+++ canteen/src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import img6 from '../assets/img/img6.jpg';
+
 
 const Navbar = () => {
   return (
     <header className="navbar">
-      <h1 className="logo">Canteen Management System</h1>
+      <div className="logo-container">
+        <img src={img6} alt="Canteen Logo" className="logo-img" />
+        <h1 className="logo">Canteen Management System</h1>
+      </div>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
