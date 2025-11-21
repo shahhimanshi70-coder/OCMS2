c46081146c983db392e923b0470b5de1fe9aba52
shahhimanshi70-coder <shahhimanshi70@gmail.com>
2025-11-21 01:22:00 -0800

    Update CanteenHome.jsx

--- canteen/src/Components/Layouts/CanteenHome.jsx
+++ canteen/src/Components/Layouts/CanteenHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './CanteenHome.css';
-
+import img7 from '../../assets/img/img7.jpg';
 const CanteenHome = () => {
   return (
     <div className="canteen-home">
-      
+      <div className="home-left"></div>
+      <img src={img7} alt="Canteen" className="side-img" />
+  <div className="home-right"></div>
       <h2>Welcome to the Canteen</h2>
       <p>Browse the menu and place your order with ease.</p>
       <div className="home-info">
